535f029d943a0092b0a1e16a631051b0904f234b
Samuel Tardieu <sam@rfc1149.net>
2020-11-02 23:44:47 +0100

    Add tests for PDF generation

--- src/main/scala/GlobalConfig.scala
+++ src/main/scala/GlobalConfig.scala
@@ -1,4 +1,8 @@
+import org.apache.commons.io.IOUtils
+
 object GlobalConfig {
+
+  val certificate = IOUtils.resourceToByteArray("/certificate.d1673940.pdf")
 
   var help: Option[String] = None
   var privacyPolicy: Option[String] = None
